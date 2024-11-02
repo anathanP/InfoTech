80ed93b04b96ba6805b6dfee4dedd98c91587a09
Anathan <abbasmasoudardakani@gmail.com>
2024-11-02 07:49:02 +0330

    Add project files.

---
+++ InfoTechWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace InfoTechWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
