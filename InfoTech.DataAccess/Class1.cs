5bc0b55b8d77b3205748b04beacef2ae5e2641d0
Anathan <abbasmasoudardakani@gmail.com>
2024-11-02 10:57:57 +0330

    Initiation EfCore on project

--- InfoTech.DataAccess/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace InfoTech.DataAccess
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ InfoTech.DataAccess/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace InfoTech.DataAccess.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options)
+            :base(options)
+        {
+            ChangeTracker.LazyLoadingEnabled = false;
+        }
+    }
+}
